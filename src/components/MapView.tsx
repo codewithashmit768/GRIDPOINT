489d9c989a9e63860ffdda82f0797e0c4793c1e0
aravpriyadarshi <arav.priyadarshi21@gmail.com>
2026-09-20 11:58:13 +0530

    fixing map

--- src/components/MapView.tsx
+++ src/components/MapView.tsx
@@ -116,8 +116,8 @@ export const MapView: React.FC<Props> = ({
         style={{ height: '100%', width: '100%' }}
       >
         <TileLayer
-          attribution='&copy; <a href="https://carto.com/">CARTO</a>'
-          url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
 
         {showOriginal && originalCenter && (
